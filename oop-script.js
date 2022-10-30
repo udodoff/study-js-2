247c3d75d40efd4c6c09ce9972e421f883eb08a7
Ruslan Udodov <udodovr03@gmail.com>
2022-10-30 11:10:31 +0300

    rendered a cart

--- oop-script.js
+++ oop-script.js
@@ -3,7 +3,10 @@ class Pruductlist{
         this.container = container
         this.goods = []
         this._fetchProducts()
-        this.render()
+            .then(data => {
+                this.goods = data
+                this.render()
+            })
     }
 
     sumofproducts(){
@@ -15,12 +18,10 @@ class Pruductlist{
     }
 
     _fetchProducts(){
-        this.goods = [
-            {id: 1, title: 'Notebook', price: 2000},
-            {id: 2, title: 'Mouse', price: 20},
-            {id: 3, title: 'Keyboard', price: 200},
-            {id: 4, title: 'Gamepad', price: 50},
-        ]
+        const info = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses/catalogData.json'
+        return fetch(info)
+                .then(result => result.json())
+                .catch(error => console.log(error))
     }
 
     render(){
@@ -33,8 +34,8 @@ class Pruductlist{
 }
 class PruductItem{
     constructor(product, img = 'https://img.freepik.com/free-photo/close-up-hand-holding-smartphone_23-2148883473.jpg?w=740&t=st=1667023371~exp=1667023971~hmac=6cd8fdd7fe1103654b3c195acb84685d7a8287b84c499942f85a6da0b0bb9bd2'){
-        this.title = product.title
-        this.id = product.id
+        this.title = product.product_name
+        this.id = product.id_product
         this.price = product.price
         this.img = img
     }
@@ -51,6 +52,21 @@ let list = new Pruductlist()
 
 
 class Cart{
+    constructor(basketContainer = '.basket'){
+        this.basketContainer = basketContainer
+        this.basketGoods = []
+        this._fetchBasketProducts()
+            .then(result => {
+                this.basketGoods = result.contents
+                this.render()
+            })
+    }
+    _fetchBasketProducts(){
+        const basketItems = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses/getBasket.json'
+        return fetch(basketItems)
+                 .then(items => items.json())
+                 .catch(error => console.log(error))
+    }
     addGood(){
 
     }
@@ -61,13 +77,36 @@ class Cart{
 
     }
     render(){
-
+        const block = document.querySelector(this.basketContainer)
+        this.basketGoods.forEach(item => {
+            let basketPruduct = new CartProduct(item)
+            block.insertAdjacentHTML("beforeend", basketPruduct.render())
+        })
     }
 }
 
 class CartProduct{
-    render(){
-        
+    constructor(productBasket, img = 'https://img.freepik.com/free-photo/close-up-hand-holding-smartphone_23-2148883473.jpg?w=740&t=st=1667023371~exp=1667023971~hmac=6cd8fdd7fe1103654b3c195acb84685d7a8287b84c499942f85a6da0b0bb9bd2'){
+        this.title = productBasket.product_name
+        this.id = productBasket.id_product
+        this.quantity = productBasket.quantity
+        this.price = productBasket.price
+        this.img = img
     }
-    
+    render(){
+        return `<div class="basket-item">
+                    <img class="item-basket-photo" src="${this.img}">
+                    <div class="item-description">
+                        <h3 class="basket-heading">${this.title}</h3>
+                        <p class="basket-quantity">${this.quantity} шт</p>
+                        <p class="basket-parag">$${this.price}</p>
+                    </div>
+                </div>`
+    }
 }
+let cart = new Cart()
+const cartBlockEl = document.querySelector('.basket')
+console.log(cartBlockEl);
+document.querySelector('.cart-button').addEventListener('click', event => {
+    cartBlockEl.classList.toggle('none')
+})
